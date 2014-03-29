3f08915e3c99e1700fe24ac0a47a3d8683548b8c
Svetla <sv.georgieva@gmail.com>
2014-03-29 18:14:30 +0200

    the class for animals added, and the empty file for testing it

---
+++ animals.py
@@ -0,0 +1,17 @@
+class Animals():
+    """docstring for Animals"""
+    def __init__(self, species, age, name, gender, weight):
+        self.species = species
+        self.age = age
+        self.name = name
+        self.gender = gender
+        self.weight = weight
+
+    def grow(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def die(self):
+        pass
